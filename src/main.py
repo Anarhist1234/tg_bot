8ec484843bfc980aad8ff39b88957f06a284c96a
amyagkov <a.myagkov@caltat.ru>
2023-12-21 18:18:59 +0300

    bot

--- src/main.py
+++ src/main.py
@@ -23,14 +23,14 @@ if __name__ == "__main__":
 
         for item in items:
             logger.info(f"{item['name']} - {item['count']}")
-            if item["count"] <= 50:
+            if item["count"] <= 20:
                 text = f"В проекте {item['name']} осталось номеров: {item['count']}"
                 api_url = f'https://api.telegram.org/bot{TelegramConfig.access_token}/sendMessage'
-                params = {'chat_id': "-784866469", 'text': text}
+                params = {'chat_id': "-1002117048282", 'text': text}
 
                 response = requests.post(api_url, params=params)
                 result = response.json()
 
                 logger.info(f"send_mes, {result}")
 
-        time.sleep(60)
+        time.sleep(60 * 10)
